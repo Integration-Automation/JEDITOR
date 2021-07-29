e5497ac8f5189d95f61dab14a8e24959dabd97e7
JE-Chen <zenxcvwait@gmail.com>
2021-07-30 02:58:34 +0800

    add run on cmd

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import subprocess
 import sys
 from contextlib import redirect_stdout
 from io import StringIO
@@ -17,14 +18,23 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = Ui_main_window()
         self.ui.setupUi(self)
-        self.ui.test_button.clicked.connect(self.test_exec)
+        self.ui.exec_button.clicked.connect(self.exec)
+        self.ui.cmd_button.clicked.connect(self.cmd)
 
-    def test_exec(self):
+    def exec(self):
         try:
             exec_string_io = StringIO()
             with redirect_stdout(exec_string_io):
                 exec(self.ui.code_edit.toPlainText())
             exec_result = exec_string_io.getvalue()
+            self.ui.console.setPlainText(exec_result)
+        except Exception as error:
+            self.ui.console.setPlainText(str(error))
+            raise error
+
+    def cmd(self):
+        try:
+            exec_result = subprocess.check_output(self.ui.code_edit.toPlainText(), shell=True).decode("utf8")
             self.ui.console.setPlainText(exec_result)
         except Exception as error:
             self.ui.console.setPlainText(str(error))
