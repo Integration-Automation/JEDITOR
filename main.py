edd75f21fd4557b545b88e1245f853366e48c5cb
JE-Chen <zenxcvwait@gmail.com>
2021-07-29 20:17:02 +0800

    add console

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import sys
+from contextlib import redirect_stdout
+from io import StringIO
+
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QMainWindow
+
 from editor import Ui_main_window
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
 
 
 class MainWindow(QMainWindow):
@@ -9,9 +17,22 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = Ui_main_window()
         self.ui.setupUi(self)
+        self.ui.test_button.clicked.connect(self.test_exec)
+
+    def test_exec(self):
+        try:
+            exec_string_io = StringIO()
+            with redirect_stdout(exec_string_io):
+                exec(self.ui.code_edit.toPlainText())
+            exec_result = exec_string_io.getvalue()
+            self.ui.console.setPlainText(exec_result)
+        except Exception as error:
+            self.ui.console.setPlainText(str(error))
+            raise error
 
 
 if __name__ == '__main__':
+    sys.excepthook = except_hook
     app = QApplication([])
     window = MainWindow()
     window.show()
