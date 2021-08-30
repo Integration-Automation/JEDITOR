271ae11ba2d98e652c34c89f74784c3c243530cb
JE-Chen <zenxcvwait@gmail.com>
2021-08-30 23:53:44 +0800

    fix tag keyword
    now can tag many word

--- je_editor/utils/code_tag/tag_keyword.py
+++ je_editor/utils/code_tag/tag_keyword.py
@@ -1,6 +1,6 @@
 from je_editor.utils.code_tag.keyword_list import keyword_list
 from je_editor.utils.theme.theme import Theme
-
+from tkinter import IntVar
 
 class HighlightText(object):
 
@@ -13,12 +13,16 @@ class HighlightText(object):
         self.tkinter_text.bind("<KeyRelease>", self.search)
 
     def search(self, event):
-        position = '1.0'
+        tag = "temp"
+        for tag in self.tkinter_text.tag_names():
+            self.tkinter_text.tag_remove(tag, self.start_position, self.end_position)
+        count_var = IntVar()
         for word in keyword_list:
+            position = '1.0'
             self.tkinter_text.tag_config(word, foreground=self.theme.tag_keyword_color)
-            while True:
-                find_function_index = self.tkinter_text.search("\y" + word + "\y", position, self.end_position, regexp=True)
+            while self.tkinter_text.compare(position, "<", "end"):
+                find_function_index = self.tkinter_text.search("\m" + word + "\M", position, self.end_position, count=count_var, regexp=True)
                 if not find_function_index:
                     break
                 position = '{}+{}c'.format(find_function_index, len(word))
-                self.tkinter_text.tag_add(word, find_function_index, position)
+                self.tkinter_text.tag_add(tag, find_function_index, position)
