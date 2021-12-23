151944150ef07510511dac369b859c8eb8d0be9d
JE-Chen <zenxcvwait@gmail.com>
2021-12-23 11:05:25 +0800

    test

--- circle_ci_test_stable/exec/code_exec_test/execute_test.py
+++ circle_ci_test_stable/exec/code_exec_test/execute_test.py
@@ -4,8 +4,8 @@ import sys
 from je_editor import exec_code
 from je_editor import JEditorExecException
 
-print(os.getcwd() + "/circle_ci_test_stable/exec/code_exec_test/execute_test.py")
-exec_result = exec_code(os.getcwd() + "circle_ci_test_stable/exec/code_exec_test/execute_test.py")
+print(os.getcwd() + "/circle_ci_test_stable/exec/code_exec_test/get_exec_test.py")
+exec_result = exec_code(os.getcwd() + "circle_ci_test_stable/exec/code_exec_test/get_exec_test.py")
 print(exec_result[0])
 print(exec_result[1], file=sys.stderr)
 
