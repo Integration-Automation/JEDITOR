d2f0b58ab7a6b77280682cb46549e7d0ad6e708a
JE-Chen <zenxcvwait@gmail.com>
2022-01-17 00:07:40 +0800

    fix test
    fix test

--- test/exec/shell_exec_test/shell_test.py
+++ test/exec/shell_exec_test/shell_test.py
@@ -1,4 +1,4 @@
-from je_editor.ui.ui_event.text_process import run_on_shell
+from je_editor.ui.ui_event.text_process.shell.shell_text import run_on_shell
 shell_run_result = run_on_shell("Help")
 print(shell_run_result[0])
 print(shell_run_result[1])
