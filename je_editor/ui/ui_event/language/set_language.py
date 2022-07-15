6adb37965082e9c8c63fcd711f50045e9ab9e675
JE-Chen <zenxcvwait@gmail.com>
2022-07-15 15:39:40 +0800

    fix an content error
    fix an content error

--- je_editor/ui/ui_event/language/set_language.py
+++ je_editor/ui/ui_event/language/set_language.py
@@ -6,5 +6,8 @@ def set_language(exec_manager, language):
     :param exec_manager: which exec manager change program language
     :param language: set exec manager program language
     """
-    exec_manager.program_language = language
+    if type(language) is list:
+        exec_manager.program_language = language[0]
+    else:
+        exec_manager.program_language = language
     editor_data_dict["language"] = language
