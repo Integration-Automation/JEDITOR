86a20d1a8ecd8f7fcdd38796c296296831524723
JE-Chen <zenxcvwait@gmail.com>
2022-07-22 01:41:26 +0800

    Update editor_with_theme.py

--- test/unit_test/tkinter_test/editor/editor_with_theme.py
+++ test/unit_test/tkinter_test/editor/editor_with_theme.py
@@ -3,5 +3,4 @@ from tkinter import ttk
 
 style = ttk.Style()
 print(style.theme_names())
-# editor = start_editor(use_theme="clam", **{"debug": True})
-editor = start_editor(use_theme="clam")
+editor = start_editor(use_theme="clam", **{"debug": True})
