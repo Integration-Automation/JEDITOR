d75965325bfc576c1a6ef447f3c2e9f12f4d4a4c
JeffreyChen <33644111+JE-Chen@users.noreply.github.com>
2023-04-26 15:24:25 +0800

    Prepare to build help menu
    Prepare to build help menu

---
+++ je_editor/pyside_ui/menu/menu_bar/help_menu/build_help_menu.py
@@ -0,0 +1,5 @@
+from PySide6.QtWidgets import QMainWindow
+
+
+def set_help_menu(ui_we_want_to_set: QMainWindow):
+    pass
--- je_editor/pyside_ui/menu/menu_bar/set_menu_bar.py
+++ je_editor/pyside_ui/menu/menu_bar/set_menu_bar.py
@@ -12,6 +12,7 @@ def set_menu_bar(ui_we_want_to_set: QMainWindow):
     ui_we_want_to_set.run_menu = ui_we_want_to_set.menu.addMenu("Run")
     ui_we_want_to_set.text_menu = ui_we_want_to_set.menu.addMenu("Text")
     ui_we_want_to_set.check_menu = ui_we_want_to_set.menu.addMenu("Check Code Style")
+    ui_we_want_to_set.help_menu = ui_we_want_to_set.menu.addMenu("Help")
     ui_we_want_to_set.setMenuBar(ui_we_want_to_set.menu)
     set_file_menu(ui_we_want_to_set)
     set_run_menu(ui_we_want_to_set)
