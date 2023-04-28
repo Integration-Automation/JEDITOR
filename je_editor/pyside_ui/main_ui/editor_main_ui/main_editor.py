1b35cadf06f42c73bb86f44b25b316b0a607891c
JeffreyChen <33644111+JE-Chen@users.noreply.github.com>
2023-04-28 10:18:41 +0800

    Add reformat json
    Add reformat json

--- je_editor/pyside_ui/main_ui/editor_main_ui/main_editor.py
+++ je_editor/pyside_ui/main_ui/editor_main_ui/main_editor.py
@@ -95,7 +95,7 @@ class EditorMain(QMainWindow):
         if self.auto_save_thread is not None:
             self.auto_save_thread.text_to_write = self.code_edit.toPlainText()
         if not redirect_manager_instance.std_out_queue.empty():
-            output_message = redirect_manager_instance.std_out_queue.get_nowait()
+            output_message = redirect_manager_instance.std_out_queue.get()
             output_message = str(output_message).strip()
             if output_message:
                 self.code_result.append(output_message)
