22e08d0d7b980b61d38d045ae7f5d60558721019
JeffreyChen <33644111+JE-Chen@users.noreply.github.com>
2023-05-03 18:03:20 +0800

    Add activate
    Add activate

--- je_editor/pyside_ui/menu/menu_bar/venv_menu/build_venv_menu.py
+++ je_editor/pyside_ui/menu/menu_bar/venv_menu/build_venv_menu.py
@@ -28,6 +28,60 @@ def set_venv_menu(ui_we_want_to_set: QMainWindow):
         lambda: pip_install_package(ui_we_want_to_set)
     )
     ui_we_want_to_set.venv_menu.addAction(ui_we_want_to_set.venv_menu.pip_action)
+    # Activate
+    ui_we_want_to_set.venv_menu.activate_menu \
+        = ui_we_want_to_set.venv_menu.addMenu("Activate Venv")
+    activate_menu = ui_we_want_to_set.venv_menu.activate_menu
+    if sys.platform in ["win32", "cygwin", "msys"]:
+        activate_menu.activate_cmd_action = QAction("Activate using cmd")
+        activate_menu.activate_cmd_action.triggered.connect(
+            lambda: activate_venv(
+                activate_command="",
+                path="/venv/Scripts/activate.bat"
+            )
+        )
+        activate_menu.addAction(activate_menu.activate_cmd_action)
+        activate_menu.activate_power_shell_action = QAction("Activate using Power Shell")
+        activate_menu.activate_power_shell_action.triggered.connect(
+            lambda: activate_venv(
+                activate_command="",
+                path="/venv/Scripts/Activate.ps1"
+            )
+        )
+        activate_menu.addAction(activate_menu.activate_power_shell_action)
+    else:
+        activate_menu.activate_bash_action = QAction("Activate using bash")
+        activate_menu.activate_bash_action.triggered.connect(
+            lambda: activate_venv(
+                activate_command="source ",
+                path="/venv//bin/activate"
+            )
+        )
+        activate_menu.addAction(activate_menu.activate_bash_action)
+        activate_menu.activate_fish_action = QAction("Activate using fish")
+        activate_menu.activate_fish_action.triggered.connect(
+            lambda: activate_venv(
+                activate_command="source ",
+                path="/venv/bin/activate.fish"
+            )
+        )
+        activate_menu.addAction(activate_menu.activate_fish_action)
+        activate_menu.activate_csh_action = QAction("Activate using csh")
+        activate_menu.activate_csh_action.triggered.connect(
+            lambda: activate_venv(
+                activate_command="source ",
+                path="/venv/bin/activate.csh"
+            )
+        )
+        activate_menu.addAction(activate_menu.activate_csh_action)
+        activate_menu.activate_posix_power_shell_action = QAction("Activate using Posix Power Shell")
+        activate_menu.activate_posix_power_shell_action.triggered.connect(
+            lambda: activate_venv(
+                activate_command="",
+                path="/venv/bin/Activate.ps1"
+            )
+        )
+        activate_menu.addAction(activate_menu.activate_posix_power_shell_action)
 
 
 def create_venv(ui_we_want_to_set: QMainWindow):
@@ -71,3 +125,9 @@ def pip_install_package(ui_we_want_to_set: QMainWindow):
             else:
                 compiler_path = shutil.which(cmd="python")
             shell_manager.exec_shell(f"{compiler_path} -m pip install {package_text}")
+
+
+def activate_venv(activate_command: str, path: str):
+    activate_path = Path(os.getcwd() + path)
+    print(activate_command + str(activate_path))
+    shell_manager.exec_shell(f"{activate_command} {str(activate_path)}")
