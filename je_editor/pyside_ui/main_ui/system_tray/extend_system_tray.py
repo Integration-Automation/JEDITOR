cc6b99b4f1ff5e3ac5f0d6256f5e39156264660e
JeffreyChen <zenmailman@gmail.com>
2023-07-26 14:24:38 +0800

    Update dev version
    Update dev version

---
+++ je_editor/pyside_ui/main_ui/system_tray/extend_system_tray.py
@@ -0,0 +1,31 @@
+import sys
+
+from PySide6.QtGui import QAction
+from PySide6.QtWidgets import QSystemTrayIcon, QMenu, QMainWindow
+
+
+class ExtendSystemTray(QSystemTrayIcon):
+
+    def __init__(self, main_window: QMainWindow):
+        super().__init__(parent=main_window)
+        self.menu = QMenu()
+        self.main_window = main_window
+        self.hide_main_window_action = QAction("Hide")
+        self.hide_main_window_action.triggered.connect(self.main_window.hide)
+        self.menu.addAction(self.hide_main_window_action)
+        self.maximized_main_window_action = QAction("Maximized")
+        self.maximized_main_window_action.triggered.connect(self.main_window.showMaximized)
+        self.menu.addAction(self.maximized_main_window_action)
+        self.normal_main_window_action = QAction("Normal")
+        self.normal_main_window_action.triggered.connect(self.main_window.showNormal)
+        self.menu.addAction(self.normal_main_window_action)
+        self.close_main_window_action = QAction("Close")
+        self.close_main_window_action.triggered.connect(self.close_all)
+        self.menu.addAction(self.close_main_window_action)
+        self.setContextMenu(self.menu)
+
+    def close_all(self):
+        self.setVisible(False)
+        self.main_window.close()
+        sys.exit(0)
+
