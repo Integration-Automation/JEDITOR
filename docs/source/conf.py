1b399ff364022b90bf2acdf1ac8fbea53bd70e51
JE-Chen45 <zenjeaoaruche@gmail.com>
2023-08-11 17:33:50 +0800

    Edit doc theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ sys.path.insert(0, str(Path(__file__).parent.parent))
 
 
 project = "JEditor"
-copyright = "2020~2023, JE-Chen"
+copyright = "2020 ~ Now, JE-Chen"
 author = "JE-Chen"
 
 # The full version, including alpha/beta/rc tags
@@ -48,7 +48,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "groundwork"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
